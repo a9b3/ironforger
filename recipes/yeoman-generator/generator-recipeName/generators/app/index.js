fab1b15701f7ab8f143ecf47e5d594f7e1a4b2ef
Sam Lau <sam.suifai.lau@gmail.com>
2015-08-05 01:32:41 -0700

    yeoman-generator

--- recipes/yeoman-generator/generator-recipeName/generators/app/index.js
+++ recipes/yeoman-generator/generator-recipeName/generators/app/index.js
@@ -1,10 +1,13 @@
 'use strict';
+// Deps
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
 
 module.exports = yeoman.generators.Base.extend({
+
     prompting: function () {
+
         var done = this.async();
 
         // Have Yeoman greet the user.
@@ -12,6 +15,7 @@ module.exports = yeoman.generators.Base.extend({
             'Welcome to the sensational ' + chalk.red('recipeName') + ' generator!'
         ));
 
+        // Prompt for generator args
         var prompts = [
             {
                 name: 'appname',
@@ -25,74 +29,35 @@ module.exports = yeoman.generators.Base.extend({
             // To access props later use this.props.someOption;
             done();
         }.bind(this));
+
     },
 
     writing: {
+
         app: function () {
+
+            // get generator args through 'this'
             var context = {
                 appname: this.appname
             };
 
-            this.template('_bower.json', 'bower.json', context);
-            this.template('_package.json', 'package.json', context);
-            this.template('_gulpfile.js', 'gulpfile.js', context);
-            this.template('gitignore', '.gitignore', context);
+            // copy file using context as argument
+            // this.template('_bower.json', 'bower.json', context);
 
-            this.fs.copyTpl(
-                this.templatePath('src/index.html'), 
-                this.destinationPath('src/index.html'), 
-                context);
+            // this.fs.copyTpl(
+            //     this.templatePath('src/index.html'), 
+            //     this.destinationPath('src/index.html'), 
+            //     context);
 
-            this.fs.copyTpl(
-                this.templatePath('src/scripts/app.module.js'), 
-                this.destinationPath('src/scripts/app.module.js'), 
-                context);
+            // copy no arguments passed in
+            // this.fs.copy(
+            //     this.templatePath('src/scripts/gulp.templateCache.module.js'), 
+            //     this.destinationPath('src/scripts/gulp.templateCache.module.js'));
 
-            this.fs.copy(
-                this.templatePath('src/scripts/gulp.templateCache.module.js'), 
-                this.destinationPath('src/scripts/gulp.templateCache.module.js'));
-            this.fs.copy(
-                this.templatePath('src/scripts/app.controller.js'), 
-                this.destinationPath('src/scripts/app.controller.js'));
-            this.fs.copy(
-                this.templatePath('src/scripts/app.config.js'), 
-                this.destinationPath('src/scripts/app.config.js'));
-
-            this.fs.copy(
-                this.templatePath('src/scripts/shared/app.shared.module.js'), 
-                this.destinationPath('src/scripts/shared/app.shared.module.js'));
-            this.fs.copy(
-                this.templatePath('src/scripts/components/app.components.module.js'), 
-                this.destinationPath('src/scripts/components/app.components.module.js'));
-            this.fs.copy(
-                this.templatePath('src/scripts/views/app.views.module.js'), 
-                this.destinationPath('src/scripts/views/app.views.module.js'));
-
-            this.fs.copyTpl(
-                this.templatePath('src/styles/index.scss'), 
-                this.destinationPath('src/styles/index.scss'), 
-                context);
-
-            this.fs.copyTpl(
-                this.templatePath('src/styles/vendor.scss'), 
-                this.destinationPath('src/styles/vendor.scss'), 
-                context);
-
-            this.fs.copyTpl(
-                this.templatePath('src/js/hello.js'), 
-                this.destinationPath('src/js/hello.js'), 
-                context);
-
-            this.fs.copy(
-                this.templatePath('src/images/hello.png'), 
-                this.destinationPath('src/images/hello.png'));
-
-            this.fs.copy(
-                this.templatePath('src/vendor/vendorExample.css'), 
-                this.destinationPath('src/vendor/vendorExample.css'));
         },
 
         projectfiles: function () {
+
             this.fs.copy(
                 this.templatePath('editorconfig'),
                 this.destinationPath('.editorconfig')
@@ -101,11 +66,13 @@ module.exports = yeoman.generators.Base.extend({
                 this.templatePath('jshintrc'),
                 this.destinationPath('.jshintrc')
             );
+
         }
+
     },
 
     install: function () {
         this.installDependencies();
     }
-});
 
+});
